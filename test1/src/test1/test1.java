cc95c35bf0bf6b4590e5df91fd48f9e328f1878c
sjkang <sjkang@daegu.ac.kr>
2017-04-04 09:27:57 +0900

    added 'your world'

--- test1/src/test1/test1.java
+++ test1/src/test1/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 	public static void main(String [] args ){
 		System.out.println("Hello world");
 		System.out.println("Hello his world");
-		System.out.println("Hello her world");
+		
 	}
 	
 
