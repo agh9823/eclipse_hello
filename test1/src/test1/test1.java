1c254180996a721955b6ecc1b03d2951ae1a4b34
sjkang <sjkang@daegu.ac.kr>
2017-04-04 09:26:17 +0900

    add print line

--- test1/src/test1/test1.java
+++ test1/src/test1/test1.java
@@ -3,6 +3,11 @@ package test1;
 public class test1 {
 	public static void main(String [] args ){
 		System.out.println("Hello world");
+		System.out.println("Hello his world");
+		System.out.println("Hello her world");
 	}
+	
 
 }
+
+
