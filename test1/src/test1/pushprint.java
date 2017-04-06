c2ae1dd16dcf679131cb43a81daf2cee4b5e7138
sjkang <sjkang@daegu.ac.kr>
2017-04-06 11:07:41 +0900

    Update PushPrint.java in Local

--- test1/src/test1/pushprint.java
+++ test1/src/test1/pushprint.java
@@ -3,7 +3,7 @@ package test1;
 public class pushprint {
 	public static void main (String[] args) {
 			System.out.println("Add Java file");
-			System.out.println("Hello in Local...");
+			System.out.println("Hello in Local....");
 			
 	}
 
