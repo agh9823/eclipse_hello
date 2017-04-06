c00261f265a7cb01d263a3a99b766cf3a0cad5e7
agh9823 <agh9823@naver.com>
2017-04-06 11:07:07 +0900

    Update pushprint.java
    PushPrint.java modified on Github

--- test1/src/test1/pushprint.java
+++ test1/src/test1/pushprint.java
@@ -3,7 +3,7 @@ package test1;
 public class pushprint {
 	public static void main (String[] args) {
 			System.out.println("Add Java file");
-			System.out.println("Hello...");
+			System.out.println("Hello....");
 			
 	}
 
